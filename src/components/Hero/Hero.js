e7b1f34435764ef499787eca17f81b3b9f253692
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-08-10 22:08:43 +0200

    First module React

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,8 @@ import styles from './Hero.scss';
 
 const Hero = () => (
   <header className={styles.component}>
-    <h2 className={styles.subtitle}>Things to do</h2>
-    <img className={styles.picture} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>     
+    <h2 className={styles.title}>Things to do</h2>
+    <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>     
   </header>
 );
 
